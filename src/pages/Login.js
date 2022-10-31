a26b8694155e7f931b9560d2b55a1b8f415c62c0
Thiago <thidurante@hotmail.com>
2022-10-31 16:49:10 -0300

    Creates login page/Saves email to global state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+import { connect } from 'react-redux';
+import { loginEvent } from '../redux/actions';
+
+function Login(props) {
+  const { dispatch } = props;
+  const [email, setEmail] = useState(() => '');
+  const [password, setPassword] = useState(() => '');
+  const [enterBtn, setEnterBtn] = useState(() => true);
+
+  // checks if button should be active
+  useEffect(() => {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const emailCheck = emailRegex.test(email);
+    const six = 6;
+    const passwordCheck = password.length >= six;
+    if (emailCheck && passwordCheck) {
+      setEnterBtn(false);
+    } else {
+      setEnterBtn(true);
+    }
+  }, [email, password]);
+
+  const handleLoginClick = () => {
+    const { history } = props;
+    dispatch(loginEvent(email));
+    history.push('/carteira');
+  };
+
+  return (
+
+    <div>
+      <form action="">
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            value={ email }
+            id="email"
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            value={ password }
+            id="password"
+            onChange={ (e) => setPassword(e.target.value) }
+
+          />
+        </label>
+        <button
+          disabled={ enterBtn }
+          type="button"
+          onClick={ handleLoginClick }
+        >
+          Entrar
+
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,11 @@
 // Coloque aqui suas actions
+export const loginEvent = (email) => ({
+  type: 'loginEvent',
+  payload: email,
+});
+
+// placeholder for esLint
+export const loginEvent2 = (email) => ({
+  type: 'loginEvent',
+  payload: email,
+});
